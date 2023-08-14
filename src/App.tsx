bb5fce7af553ff8f44608aceb7fa791e0efeb09c
Lazar Stojanović <lakisd98@gmail.com>
2023-08-14 22:15:50 +0200

    Add: masonry image layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ function App() {
 				<Filter />
 				<AddPhoto />
 			</nav>
-			<main>
-				<Gallery />
-			</main>
+			<Gallery />
 		</>
 	);
 }
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,34 @@
-// import styles from "./Gallery.module.css";
+import { useEffect, useState } from "react";
+
+import styles from "./Gallery.module.css";
+
+const API_ENDPOINT = "https://my-unsplash-api-pi.vercel.app/photos";
+
+type Image = {
+	id: string;
+	url: string;
+	label: string;
+};
 
 export default function Gallery() {
-	return <></>;
+	const [images, setImages] = useState<Image[]>([]);
+
+	useEffect(() => {
+		const fetchImages = async () => {
+			const response = await fetch(API_ENDPOINT);
+			const data = await response.json();
+			setImages(data);
+		};
+		fetchImages();
+	}, []);
+
+	return (
+		<main className={styles.gallery}>
+			<div className={styles.card}>
+				{images.map((image) => {
+					return <img src={image.url} key={image.id} alt={image.label} />;
+				})}
+			</div>
+		</main>
+	);
 }
