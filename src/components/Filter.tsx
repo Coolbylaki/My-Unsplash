94180ef46fbeeb286ed1874c78a5a40f7d60aed7
Lazar Stojanović <lakisd98@gmail.com>
2023-08-15 14:22:26 +0200

    Remove: icon from filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -5,7 +5,6 @@ export default function Filter() {
 	return (
 		<section className={styles.main}>
 			<aside className={styles.container}>
-				<i className={"fa-solid fa-user fa-xl" + " " + styles.ico}></i>
 				<div>
 					<h3>My Unsplash</h3>
 					<p>devchallenges.io</p>
